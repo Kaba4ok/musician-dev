9d5547611b169a50cae2f91bbba7836006f34233
Petrovi4 <s.p.prohorov@gmail.com>
2021-05-01 13:47:16 +0300

    Добавляет механику анимации

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,156 @@
-let tl = gsap.timeline({paused: true});
+'use strict';
 
-function fromHeroToStoryDown(e) {
-    e.preventDefault();
+let delimiter = 0;
+const DEBOUNCE_DURATION = 3500;
 
-    tl.to('.header__logo-music-toggle-wrapper', {
-        y: -95, duration: 1,
-        ease: 'ease-in-out'
-    })
-    .to('.social', {y: -100, duration: 1}, '-=1')
-    .to('.hero__bg-layout', {opacity: 1, duration: 1}, "-=1")
-    .to('.hero__slogan', {x: -250, duration: 1}, '-=1')
-    .to('.hero__hashtag', {x: 200, duration: 1}, '-=1')
-    .to('.story__autor', {y: '-100vh', duration: 1})
-    .to('.story__title', {y: '-100vh', duration: 0.9}, '-=0.9')
-    .to('.story__text:nth-child(1)', {y: '-100vh', duration: 0.9}, '-=0.9')
-    .to('.story__text:nth-child(2)', {y: '-100vh', duration: 1}, '-=0.9')
-    .to('.story__bg-circle', {y: '-90vh', duration: 2}, '-=1');
+if (document.documentElement.clientWidth > 1200) {
 
-    tl.play();
-}
+    let screenSwitcher = {
+        screens: ['hero', 'story', 'featuresTop', 'featuresBotoom', 'facts', 'prices', 'footer'],
+        currentSreen: 'hero',
+        get lastScreenIndex () {
+            return Number(this.screens.length) - 1;
+        },
+        heroStoryAnimation: {
 
-function fromHeroToStoryUp(e) {
-    e.preventDefault();
-    tl.reverse();
-}
+            HeroStoryAnimationTL: gsap.timeline({paused: true})
+            .to('.header__logo-music-toggle-wrapper', {y: -95, duration: 1, ease: 'ease-in-out'})
+            .to('.social', {y: -100, duration: 1}, '-=1')
+            .to('.hero__bg-layout', {opacity: 1, duration: 1}, "-=1")
+            .to('.hero__slogan', {x: -250, duration: 1}, '-=1')
+            .to('.hero__hashtag', {x: 200, duration: 1}, '-=1')
+            .to('.hero', {y: '-100vh', duration: 0})
+            .to('.story', {y: '-100vh', duration: 0}, '-=0')
+            .fromTo('.story__autor', {y: '100vh'}, {y: 0, duration: 0.8})
+            .fromTo('.story__title', {y: '100vh'}, {y: 0, duration: 0.7}, '-=0.8')
+            .fromTo('.story__text:nth-child(1)', {y: '100vh'}, {y: 0, duration: 0.9}, '-=0.8')
+            .fromTo('.story__text:nth-child(2)', {y: '100vh'}, {y: 0, duration: 1}, '-=0.9')
+            .fromTo('.story__bg-circle', {y: '100vh'}, {y: 80, duration: 2}, '-=1'),
 
+            down() {
+                console.log('hero_DOWN');
+                this.HeroStoryAnimationTL.play();
+            },
+            up() {
+                console.log('hero_UP');
+                this.HeroStoryAnimationTL.reverse();
+            }
+        },
+        storyFeaturesTopAnimation: {
 
-document.addEventListener('wheel', function(e) {
-    if(e.deltaY > 0) {
-        fromHeroToStoryDown(e);
-    } else {
-        fromHeroToStoryUp(e);
+            storyFeaturesTopAnimationTL: gsap.timeline({paused: true})
+            .to('.hero-story-wrapper', {y: '-100vh', duration: 2})
+            .to('.features', {y: '-200vh', duration: 2}, '-=2'),
+
+            down() {
+                console.log('storyFeaturesTopAnimation_DOWN');
+                this.storyFeaturesTopAnimationTL.play();
+            },
+            up() {
+                this.storyFeaturesTopAnimationTL.reverse();
+            }
+        },
+        storyFeaturesTopFeaturesBottomAnimation: {
+            down() {
+                console.log('storyFeaturesTopFeaturesBottomAnimation_DOWN');
+            },
+            up() {
+                console.log('storyFeaturesTopFeaturesBottomAnimation_UP');
+            }
+        },
+        featuresBottomFactsAnimation: {
+            down() {
+                console.log('featuresBottomFactsAnimation_DOWN');
+            },
+            up() {
+                console.log('featuresBottomFactsAnimation_UP');
+            }
+        },
+        factsPricesAnimation: {
+            down() {
+                console.log('factsPricesAnimation_DOWN');
+            },
+            up() {
+                console.log('factsPricesAnimation_UP');
+            }
+        },
+        pricesFooterAnimation: {
+            down() {
+                console.log('pricesFooterAnimation_DOWN');
+            },
+            up() {
+                console.log('pricesFooterAnimation_UP');
+            }
+        },
     }
-}, { passive: false });
+
+    window.addEventListener('wheel', function(e) {
+
+        const screenIndex = screenSwitcher.screens.indexOf(screenSwitcher.currentSreen);
+
+        if(e.deltaY > 0) {
+            if(delimiter === 0) {
+
+                delimiter = 1;
+
+                switch (screenSwitcher.currentSreen) {
+                    case "hero":
+                        screenSwitcher.heroStoryAnimation.down();
+                        break;
+                    case "story":
+                        screenSwitcher.storyFeaturesTopAnimation.down();
+                        break;
+                    case "featuresTop":
+                        screenSwitcher.storyFeaturesTopFeaturesBottomAnimation.down();
+                        break;
+                    case "featuresBottom":
+                        screenSwitcher.featuresBottomFactsAnimation.down();
+                        break;
+                    case "facts":
+                        screenSwitcher.factsPricesAnimation.down();
+                        break;
+                    case "prices":
+                        screenSwitcher.pricesFooterAnimation.down();
+                        break;
+                }
+
+                setTimeout(() => {
+                    delimiter = 0;
+                }, DEBOUNCE_DURATION);
+
+                screenSwitcher.currentSreen = screenIndex === screenSwitcher.lastScreenIndex ? "hero" : screenSwitcher.screens[screenIndex + 1];
+            }
+        } else {
+            if(delimiter === 0) {
+                delimiter = 1;
+
+                screenSwitcher.currentSreen = screenIndex === 0 ? screenSwitcher.lastScreenIndex : screenSwitcher.screens[screenIndex - 1];
+                
+                switch (screenSwitcher.currentSreen) {
+                    case "hero":
+                        screenSwitcher.heroStoryAnimation.up();
+                        break;
+                    case "story":
+                        screenSwitcher.storyFeaturesTopAnimation.up();
+                        break;
+                    case "featuresTop":
+                        screenSwitcher.storyFeaturesTopFeaturesBottomAnimation.up();
+                        break;
+                    case "featuresBottom":
+                        screenSwitcher.featuresBottomFactsAnimation.up();
+                        break;
+                    case "facts":
+                        screenSwitcher.factsPricesAnimation.up();
+                        break;
+                    case "prices":
+                        screenSwitcher.pricesFooterAnimation.up();
+                        break;
+                }
+
+                setTimeout(() => {
+                    delimiter = 0;
+                }, DEBOUNCE_DURATION);
+            }
+        }
+    });
+}
